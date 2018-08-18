babda8e12d53909f15b5927b5554d20fb5b3e403
Mauro Monti <mro.monti@gmail.com>
2018-08-18 16:09:48 -0300

    quitar archivos

--- iniciar_inmobiliaria.sh
+++
@@ -1,7 +0,0 @@
-git pull
-python manage.py makemigrations contratos
-python manage.py makemigrations parametros
-python manage.py makemigrations personas
-python manage.py makemigrations propiedades
-python manage.py migrate
-gunicorn -w 4 inmobiliaria.wsgi
